ef521fe60aa26771679e46720fc3cce7d62a357f
Linda Ho <linda.oanh.ho@gmail.com>
2017-10-02 21:09:58 -0500

    Refactor with parameterized tests

--- src/test/java/cs271/lab/list/TestPerformance.java
+++ src/test/java/cs271/lab/list/TestPerformance.java
@@ -1,46 +1,48 @@
 package cs271.lab.list;
 
 import java.util.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
 
+@RunWith(Parameterized.class)
 public class TestPerformance {
 
     // Done run test and record running times for SIZE = 10, 100, 1000, 10000, ...
     // (choose in conjunction with REPS below up to an upper limit where the clock
     // running time is in the tens of seconds)
 
-    // TODO refactor to DRY
+    // Done refactor to DRY
     // which of the two lists performs better as the size increases?
-    //private final int SIZE = 10;
-    private final int SIZE = 100;
-    //private final int SIZE = 1000;
-    //private final int SIZE = 10000;
+    //
+    private int size;
 
-    // done choose this value in such a way that you can observe an actual effect
+    public TestPerformance(int size) {
+        this.size = size;
+    }
+
+    @Parameterized.Parameters
+    public static List<Integer> data() {
+        return Arrays.asList(10, 100, 1000, 10000, 100000);
+    }
+
+    // Done choose this value in such a way that you can observe an actual effect
     // for increasing problem sizes
 
-    private final int REPS = 1000000;
-    //private final int REPS = 10000000;
-    //private final int REPS = 100000000;
-
+    private final int REPS = 100000;
+    //private final int REPS = 1000000;
+    
     private List<Integer> arrayList;
 
     private List<Integer> linkedList;
 
-    private long totalTime;
-
-    public void calculateRunTime(final long startTime, final long endTime) {
-        totalTime = endTime - startTime;
-    }
-
     @Before
     public void setUp() throws Exception {
-        arrayList = new ArrayList<Integer>(SIZE);
+        arrayList = new ArrayList<Integer>(size);
         linkedList = new LinkedList<Integer>();
-        for (int i = 0; i < SIZE; i++) {
+        for (int i = 0; i < size; i++) {
             arrayList.add(i);
             linkedList.add(i);
         }
@@ -54,49 +56,33 @@ public class TestPerformance {
 
     @Test
     public void testLinkedListAddRemove() {
-        final long startTime = System.currentTimeMillis();
         for (int r = 0; r < REPS; r++) {
             linkedList.add(0, 77);
             linkedList.remove(0);
         }
-        final long endTime   = System.currentTimeMillis();
-        calculateRunTime(startTime, endTime);
-        System.out.println("LinkedList AddRemove with size of " + SIZE + " took: " + totalTime);
     }
 
     @Test
     public void testArrayListAddRemove() {
-        final long startTime = System.currentTimeMillis();
         for (int r = 0; r < REPS; r++) {
             arrayList.add(0, 77);
             arrayList.remove(0);
         }
-        final long endTime = System.currentTimeMillis();
-        calculateRunTime(startTime, endTime);
-        System.out.println("ArrayList AddRemove with size of " + SIZE + " took: " + totalTime);
     }
 
     @Test
     public void testLinkedListAccess() {
-        final long startTime = System.currentTimeMillis();
         long sum = 0;
         for (int r = 0; r < REPS; r++) {
-            sum += linkedList.get(r % SIZE);
+            sum += linkedList.get(r % size);
         }
-        final long endTime = System.currentTimeMillis();
-        calculateRunTime(startTime, endTime);
-        System.out.println("LinkedList Access with size of " + SIZE + " took: " + totalTime);
     }
 
     @Test
     public void testArrayListAccess() {
-        final long startTime = System.currentTimeMillis();
         long sum = 0;
         for (int r = 0; r < REPS; r++) {
-            sum += arrayList.get(r % SIZE);
+            sum += arrayList.get(r % size);
         }
-        final long endTime = System.currentTimeMillis();
-        calculateRunTime(startTime, endTime);
-        System.out.println("ArrayList Access with size of " + SIZE + " took: " + totalTime);
     }
 }
